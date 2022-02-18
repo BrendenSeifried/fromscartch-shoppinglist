c1673c96c4fdebb2fd0619010fef2d8a930d4451
BrendenSeifried <brendenseifried@gmail.com>
2022-02-18 15:55:26 -0800

    test complete

--- test/render-utils.test.js
+++ test/render-utils.test.js
@@ -1,16 +1,17 @@
 // IMPORT MODULES under test here:
-//import { renderItem } from '../render-utils.js';
+//import { completeItem } from '../fetch-utils.js';
+import { renderItem } from '../render-utils.js';
 
 const test = QUnit.test;
 
 test('time to test a function', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<li>test</li>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderItem({ description: 'test', complete: false });
 
     //Expect
     // Make assertions about what is expected versus the actual result
